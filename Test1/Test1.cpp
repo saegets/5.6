24c51cba3415b60307722997686450248729a911
saegets <mklimasevskij@gmail.com>
2020-12-09 22:20:45 +0200

    test

---
+++ Test1/Test1.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Test1
+{
+	TEST_CLASS(Test1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
